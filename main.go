c2eb7afa6324b88529fd23b0c03624233b99b61d
Eric Lubow <eric@lubow.org>
2016-11-17 16:42:28 -0500

    Code builds with v2 data files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package fiftyonedegrees
+package main
 
 /*
+#cgo CFLAGS: -I . -Wimplicit-function-declaration
 #include "51Degrees.h"
 */
 import "C"
@@ -9,6 +10,19 @@ import (
 	"fmt"
 	"unsafe"
 )
+
+func main() {
+	properties := "DeviceType, IsMobile, IsSmartPhone, IsTablet, IsTv, HardwareName, HardwareVendor, HardwareModel, BrowserName, BrowserVersion, PlatformName, PlatformVersion, ScreenPixelsWidth, ScreenPixelsHeight"
+	testUA := "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
+	//item, err := NewFiftyoneDegrees("51Degrees-EnterpriseV3_1.dat", properties)
+	item, err := NewFiftyoneDegrees("51Degrees-PremiumV3_2.dat", properties)
+	if err != nil {
+		fmt.Println("err=", err)
+		return
+	}
+	fmt.Println(item.Parse(testUA))
+	item.Close()
+}
 
 type FiftyoneDegrees struct {
 	dataSet *C.fiftyoneDegreesDataSet
@@ -27,12 +41,12 @@ func (this *FiftyoneDegrees) Close() {
 }
 
 func (this *FiftyoneDegrees) Parse(userAgent string) string {
-	ws:=C.fiftyoneDegreesCreateWorkset(this.dataSet)
+	ws := C.fiftyoneDegreesCreateWorkset(this.dataSet)
 	defer C.fiftyoneDegreesFreeWorkset(ws)
 	C.fiftyoneDegreesMatch(ws, C.CString(userAgent))
 	resultLength := 50000
 	buff := make([]byte, resultLength)
-	length := int32(C.fiftyoneDegreesProcessDeviceJSON(ws, (*C.char)(unsafe.Pointer(&buff[0])), C.int32_t(resultLength)))
+	length := int32(C.fiftyoneDegreesProcessDeviceJSON(ws, (*C.char)(unsafe.Pointer(&buff[0]))))
 	result := buff[:length]
 	return string(result)
 }
